dc89d275268e3c12094427c9f765529da0eb4bdb
salak96 <lambangsasangka@gmail.com>
2023-09-08 22:10:16 +0700

    asd

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ const {Pool} = require('pg');
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'rental_mobil',
+    database: 'rental_mobil2',
     password: 'root',
     port: 5432, // default port postgres
 })
--- seeder.js
+++ seeder.js
@@ -2,6 +2,25 @@ const pool = require('./connection');
 const fs  = require('fs');
 
 const categoriesData = fs.readFileSync('./data/categories.json', "utf-8");
+const parseCategories = JSON.parse(categoriesData);
 
-console.log(categoriesData);
+// //console.log(parseCategories); //object
+//  console.log(categoriesData); // objecct as a string
 
+const categoriesValue = parseCategories.map(element => {
+    return `("${element.categoryName}")`;
+}).join(', \n');
+console.log(categoriesValue)
+
+pool.query(`
+    INSERT INTO "Categories" ("categoryName") 
+    VALUES ${categoriesValue}
+    `), (error, result) => {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(result);
+        console.log('table created');
+        pool.end();
+    }
+}
